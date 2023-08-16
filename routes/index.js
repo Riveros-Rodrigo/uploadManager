a57fbb81bf07b8896a315dc4fbe54130a91455bf
Rodrigo Riveros <riverosdev@gmail.com>
2023-08-16 18:57:42 -0300

    agrego middlewares

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+const express = require('express');
 const { index } = require('../controllers/indexController');
-var router = express.Router();
+const router = express.Router();
 
 /* GET home page. */
 router
-  .get('/', index);
+  .get('/', index)
 
 module.exports = router;
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,63 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const {
+  list,
+  addProductOneImage,
+  storeProductOneImage,
+  editProductOneImage,
+  updateProductOneImage,
+  deleteProductOneImage,
+  addProductMultipleImages,
+  storeProductMultipleImages,
+  detailProductMultipleImages,
+  addProductMainImage,
+  storeProductMainImage,
+  detailProductMainImage,
+  editProductMultipleImages,
+  updateProductMultipleImages,
+  deleteProductMultipleImages,
+} = require("../controllers/productsController");
+const { uploadOneImage } = require("../middlewares/upload");
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', );
+/* /products */
+router
+  .get("/", list)
+  /* una imagen */
+  .get("/addOneImage", addProductOneImage)
+  .post("/addOneImage", uploadOneImage.single("image"), storeProductOneImage)
+  .get('/editOneImage/:id',editProductOneImage)
+    .put('/updateOneImage/:id',uploadOneImage.single("image"),updateProductOneImage)
+    .delete('/deleteOneImage/:id',deleteProductOneImage)
+
+  /* varias imágenes */
+  .get("/detailMultipleImages/:id", detailProductMultipleImages)
+  .get("/addMultipleImages", addProductMultipleImages)
+  .post(
+    "/addMultipleImages",
+    uploadOneImage.array("images"),
+    storeProductMultipleImages
+  )
+  .get('/editMultipleImages/:id',editProductMultipleImages)
+    .put('/updateMultipleImages/:id',uploadOneImage.array("images"),updateProductMultipleImages)
+    .delete('/deleteMultipleImages/:id',deleteProductMultipleImages)
+
+  /* una imagen principal y varias secundarias */
+  .get("/detailMainImage/:id", detailProductMainImage)
+  .get("/addMainImage", addProductMainImage)
+  .post(
+    "/addMainImage",
+    uploadOneImage.fields([
+      {
+        name: "mainImage",
+      },
+      {
+        name: "images",
+      },
+    ]),
+    storeProductMainImage
+  );
+/*     .get('/editMainImage/:id',editProductMainImage)
+    .put('/updateMainImage/:id',updateProductMainImage)
+    .delete('/deleteMainImage/:id',deleteProductMainImage) */
 
 module.exports = router;
